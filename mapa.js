63e5332a641476d9b0136117cc88986b1aa92bb8
julioeducajlle <87338898+julioeducajlle@users.noreply.github.com>
2022-08-17 17:06:57 -0300

    Update mapa.js

--- mapa.js
+++ mapa.js
@@ -30,7 +30,7 @@ function initMap() {
   heatmap = new google.maps.visualization.HeatmapLayer({
     data: getPoints(),
     map: map,
-    radius: 23,
+    radius: 30,
   });
 
   ctaLayer = new google.maps.KmlLayer({
